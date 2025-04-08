a1647e205f89632d8fa125d2f546af453217e065
Mika Braginsky <mika.br@gmail.com>
2025-04-08 14:36:56 -0400

    add helper function to get dataset ref within on-redivis notebook

--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,10 @@ compute_age <- function(birth_month, birth_year, test_date) {
   age_days <- as.numeric(difftime(test_date, birth_date, units = "days"))
   age_days / 365.25
 }
+
+#' @export
+notebook_dataset <- function(redivis) {
+  source_table <- redivis$table("_source_")$get()
+  source_dataset <- source$properties$container$scopedReference
+  dataset_spec <- list(list(name = source_dataset, version = "current"))
+}
