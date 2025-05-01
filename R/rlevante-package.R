60393624cbb68f39e73284f82d011bb2e319bb7b
Mika Braginsky <mika.br@gmail.com>
2025-05-01 16:07:03 -0400

    add missing imports

--- R/rlevante-package.R
+++ R/rlevante-package.R
@@ -2,11 +2,11 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom dplyr .data across arrange bind_rows coalesce contains distinct
-#'   everything filter group_by if_else inner_join left_join matches mutate n
-#'   na_if relocate rename select semi_join ungroup where
+#' @importFrom dplyr .data across arrange bind_rows case_when coalesce contains
+#'   distinct everything filter group_by if_else inner_join left_join matches
+#'   mutate n na_if relocate rename select semi_join ungroup where
 #' @importFrom glue glue
 #' @importFrom purrr imap map map_chr map2_dbl list_rbind list_transpose walk
-#' @importFrom tidyr nest unnest
+#' @importFrom tidyr nest replace_na unnest
 ## usethis namespace: end
 NULL
