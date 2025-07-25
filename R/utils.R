043bd34d7be505da96a707b135faa763aa4d6822
Mika Braginsky <mika.br@gmail.com>
2025-07-25 13:48:30 -0400

    suppress warning for malformed DOBs

--- R/utils.R
+++ R/utils.R
@@ -8,7 +8,8 @@ validate_birth_year <- function(birth_year) {
 
 # compute age in years from birth month/year and date of test
 compute_age <- function(birth_month, birth_year, test_date) {
-  birth_date <- lubridate::ym(paste(birth_year, birth_month, sep = "-"))
+  # if (any(is.na(c(birth_month, birth_year, test_date)))) return(NA)
+  birth_date <- lubridate::ym(paste(birth_year, birth_month, sep = "-"), quiet = TRUE)
   age_days <- as.numeric(difftime(test_date, birth_date, units = "days"))
   age_days / 365.25
 }
