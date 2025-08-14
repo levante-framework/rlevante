997a5b24fee6beed8892f4185237dae9d53ce78c
Mika Braginsky <mika.br@gmail.com>
2025-08-14 08:27:41 -0700

    fixes to survey ID linking

--- R/surveys.R
+++ R/surveys.R
@@ -28,9 +28,9 @@ code_survey_data <- function(surveys) {
     mutate(value = if_else(.data$reverse_coded,
                            map2_dbl(.data$value, .data$values, reverse_value),
                            .data$value)) |>
-    select("dataset", "survey_type", "user_id", "child_id",
-           contains("construct"), "question_type", "variable", "variable_order",
-           "value", timestamp = "created_at")
+    select("dataset", "survey_response_id", "survey_type", "survey_part",
+           "user_id", "child_id", contains("construct"), "question_type",
+           "variable", "variable_order", "value", timestamp = "created_at")
 }
 
 
@@ -43,49 +43,52 @@ code_survey_data <- function(surveys) {
 link_surveys <- function(surveys, participants) {
 
   user_survey_data <- surveys |>
-    nest(survey_data = -c(.data$dataset, .data$survey_type, .data$timestamp,
-                          .data$user_id, .data$child_id))
-
-  survey_linked <- user_survey_data |> filter(!is.na(.data$child_id))
-  survey_unlinked <- user_survey_data |> filter(is.na(.data$child_id)) |> select(-"child_id")
+    mutate(survey_group = survey_part) |>
+    nest(survey_data = -c("dataset", "survey_type", "survey_response_id", "timestamp",
+                          "survey_group", "user_id", "child_id"))
 
   children <- participants |> rename(child_id = "user_id")
 
   # student survey -- user_id is child
-  survey_student <- survey_unlinked |>
+  survey_student <- user_survey_data |>
     filter(.data$survey_type == "student") |>
-    mutate(child_id = .data$user_id)
+    mutate(child_id = user_id)
 
   # teacher survey -- user_id is teacher
   teachers <- children |>
     filter(.data$teacher_id != "") |>
     select("dataset", "child_id", "teacher_id") |>
-    nest(children = -c(.data$dataset, .data$teacher_id))
+    nest(children = -c("dataset", "teacher_id"))
 
-  survey_teacher <- survey_unlinked |>
+  survey_teacher <- user_survey_data |>
     filter(.data$survey_type == "teacher") |>
+    select(-child_id) |>
     left_join(teachers, by = c("dataset", "user_id" = "teacher_id")) |>
-    unnest(children)
+    unnest(children) |>
+    select(-children)
 
-  # caregiver survey -- user_id is caregiver
-  caregivers <- children |>
-    filter(!is.na(.data$parent1_id)) |>
-    select("dataset", "child_id", "parent1_id") |>
-    nest(children = -c(.data$dataset, .data$parent1_id))
+  survey_household <- user_survey_data |>
+    filter(.data$survey_type == "caregiver", str_detect(survey_group, "caregiver")) |>
+    rename(survey_household = survey_data) |>
+    select(-survey_group)
 
-  survey_caregiver <- survey_unlinked |>
-    filter(.data$survey_type == "caregiver") |>
-    left_join(caregivers,
-              by = c("dataset", "user_id" = "parent1_id")) |>
-    unnest(children)
+  survey_child <- user_survey_data |>
+    filter(.data$survey_type == "caregiver", survey_group == "child_specific") |>
+    rename(survey_child = survey_data) |>
+    select(-survey_group)
+
+  survey_caregiver <- survey_child |>
+    left_join(survey_household) |>
+    mutate(survey_data = map2(survey_child, survey_household, bind_rows)) |>
+    select(-survey_child, -survey_household)
 
   # recombine separated out survey types
-  survey_combined <- bind_rows(survey_student, survey_teacher, survey_caregiver, survey_linked) |>
-    select(-"children") |>
+  survey_combined <- bind_rows(survey_student, survey_teacher, survey_caregiver) |> #, survey_linked) |>
     left_join(children |> select("dataset", "child_id", "sex", "birth_month", "birth_year"),
               by = c("dataset", "child_id")) |>
     mutate(age = compute_age(.data$birth_month, .data$birth_year, .data$timestamp)) |>
-    select(-contains("birth_"), -"timestamp") |>
+    select(-contains("birth_"), -"survey_group") |>
+    rename(survey_timestamp = timestamp) |>
     relocate(.data$survey_data, .after = everything())
 
   survey_combined |> unnest(.data$survey_data)
