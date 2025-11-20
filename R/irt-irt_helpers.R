47ed055eaab10782d6f4813a43b8cb4850dcf18b
Mika Braginsky <mika.br@gmail.com>
2025-11-19 16:14:15 -0800

    add missing irt helpers

--- R/irt-irt_helpers.R
+++ R/irt-irt_helpers.R
@@ -30,6 +30,51 @@ remove_no_var_items <- function(df, item_n_min = 1) {
     filter(.data$item_mean > 0, .data$item_mean < 1, .data$item_n > item_n_min) # need to be between 0 and 1
 }
 
+#' remove items that aren't shared across all groups
+#'
+#' @param df trial data
+#'
+#' @export
+remove_nonshared_items <- function(df) {
+  n_groups_total <- length(unique(df$group))
+
+  df |>
+    group_by(item_uid) |>
+    mutate(n_groups = n_distinct(group)) |>
+    ungroup() |>
+    filter(n_groups == n_groups_total) # need to be in N or more groups
+}
+
+#' remove items with no invariance in any single group
+#'
+#' @param df trial data
+#'
+#' @export
+remove_no_var_items_bygroup <- function(df, item_n_min = 1) {
+  df |>
+    group_by(item_inst, group) |>
+    mutate(item_mean = mean(correct, na.rm = TRUE),
+           item_n = length(correct)) |> # item means
+    group_by(item_inst) |>
+    mutate(low_var = any(item_mean == 0) | any(item_mean== 1),
+           low_n = any(item_n < item_n_min)) |>
+    ungroup() |>
+    filter(!low_var, !low_n) # need to be between 0 and 1
+}
+
+#' format data for mirt
+#'
+#' @param df trial data
+#'
+#' @export
+to_mirt_shape <- function(df) {
+  df |>
+    mutate(correct = as.numeric(correct)) |> # values to numeric
+    select(run_id, item_inst, correct) |>
+    pivot_wider(names_from = "item_inst", values_from = "correct") |> # column for each item
+    column_to_rownames("run_id") # user_id to rownames
+}
+
 #' format data for mirt
 #'
 #' @param df trial data
