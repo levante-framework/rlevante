dd5341ef48a306c338e2f5e78cd02c933a1dd737
Mika Braginsky <mika.br@gmail.com>
2026-01-20 11:39:42 -0500

    registry utils tweaks for param update pipeline

--- R/irt-registry.r
+++ R/irt-registry.r
@@ -1,23 +1,27 @@
 #' Fetch model registry file index table
 #' @keywords internal
 #' @export
-fetch_registry_table <- \() {
-  redivis::redivis$organization("levante")$dataset("levante_metadata_scoring:e97h")$table("model_registry:rqwv")$to_tibble()
+fetch_registry_table <- \(version = "current") {
+  ds <- redivis::redivis$organization("levante")$dataset(name = "levante_metadata_scoring:e97h", version = version)
+  ds$table("model_registry:rqwv")$to_tibble() |>
+    mutate(redivis_source = ds$get()$scoped_reference, .before = everything())
 }
 
 #' Fetch scoring specification table
 #' @keywords internal
 #' @export
-fetch_scoring_table <- \() {
-  redivis::redivis$organization("levante")$dataset("levante_metadata_scoring:e97h")$table("scoring_models:t416")$to_tibble()
+fetch_scoring_table <- \(version = "current") {
+  ds <- redivis::redivis$organization("levante")$dataset("levante_metadata_scoring:e97h", version = version)
+  ds$table("scoring_models:t416")$to_tibble() |>
+    mutate(redivis_source = ds$get()$scoped_reference, .before = everything())
 }
 
 #' get the scoring specification in scoring_table for a given task + dataset
 #' @keywords internal
 #'
-#' @param scoring_table tibble returned by get_scoring_table()
 #' @param score_task string indicating task
 #' @param score_dataset string indicating dataset
+#' @param scoring_table tibble returned by get_scoring_table()
 #' @return list with entries item_task, dataset, model_set, subset, itemtype, nfact, invariance
 get_model_spec <- \(score_task, score_dataset, scoring_table) {
   mod_spec <- scoring_table |> filter(.data$item_task == score_task, .data$dataset == score_dataset)
@@ -26,18 +30,18 @@ get_model_spec <- \(score_task, score_dataset, scoring_table) {
   mod_spec |> as.list()
 }
 
-#' get the model record indexed in registry_table for a given scoring specification
+#' convert model spec to filename
 #' @keywords internal
-#'
-#' @param spec list with entries item_task, dataset, model_set, subset, itemtype, nfact, invariance
-#' @param registry_table tibble returned by get_registry_table()
-#' @return ModelRecord object
-get_model_record <- \(spec, registry_table) {
-
-  # construct filename from specification
+#' @export
+model_spec_filename <- \(spec) {
   mod_basename <- spec[c("item_task", "itemtype", "nfact", "invariance")] |> purrr::discard(is.na) |> paste(collapse = "_")
   mod_filename <- glue::glue("{spec$item_task}/{spec$model_set}/{spec$subset}/{mod_basename}.rds")
+}
 
+#' fetch file from model registry
+#' @keywords internal
+#' @export
+get_registry_file <- \(mod_filename, registry_table) {
   # get file_id corresponding to filename
   mod_file <- registry_table |> filter(.data$file_name == mod_filename) |> pull("file_id")
 
@@ -53,7 +57,22 @@ get_model_record <- \(spec, registry_table) {
   mod_rec
 }
 
-# purrr::map invariance as recorded in mirt
+#' get the model record indexed in registry_table for a given scoring specification
+#' @keywords internal
+#'
+#' @param spec list with entries item_task, model_set, subset, itemtype, nfact, invariance
+#' @param registry_table tibble returned by get_registry_table()
+#' @return ModelRecord object
+get_model_record <- \(spec, registry_table) {
+
+  # construct filename from specification
+  mod_filename <- model_spec_filename(spec)
+
+  # fetch corresponding file from model registry
+  get_registry_file(mod_filename, registry_table)
+}
+
+# invariance as recorded in mirt
 # (subset of c("free_means", "free_var", "intercepts", "slopes"))
 # to shorthand name
 # (one of c("configural", "metric", "scalar_intercepts", "scalar_slopes_and_intercepts", "full")
