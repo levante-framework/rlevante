acfc8d89710cad9757dfbac0b92a7ff93e8aaef4
Mika Braginsky <mika.br@gmail.com>
2026-02-24 12:41:42 -0500

    fix args of get_raw_table

--- R/fetch-raw_data.R
+++ R/fetch-raw_data.R
@@ -1,14 +1,15 @@
 #' Get raw table
 #'
-#' @inheritParams get_participants
 #' @param table_name String indicating name of table to get
+#' @param data_source String indicating which Redivis dataset to get
+#' @param data_source_version String indicating which version of `data_source` to get
 #'
 #' @export
 #' @examples
 #' \dontrun{
-#' runs <- get_raw_table(table_name = "runs", dataset = "levante-data-example:bm7r")
+#' runs <- get_raw_table(table_name = "runs", data_source = "levante-data-example:bm7r")
 #' }
-get_raw_table <- function(table_name, data_source, version = "current") {
-  dataset_spec <- list(list(dataset = data_source, version = version))
+get_raw_table <- function(table_name, data_source, data_source_version = "current") {
+  dataset_spec <- list(list(name = data_source, version = data_source_version))
   get_datasets_data(dataset_spec, table_getter(table_name))
 }
